3bb9791302846dd8a5830f658fb8ada89342bdae
ChinonsoIg <ojononso@yahoo.com>
2021-12-05 11:32:22 +0100

    added toggle image button

--- html-test/main.js
+++ html-test/main.js
@@ -1,20 +1,94 @@
-const body = document.getElementById("body");
+const body = document.querySelector("#body");
 const paragraphs = document.getElementsByClassName("paragraph");
-const btn = document.getElementById("btn");
+const btn = document.querySelector("#btn");
+const img = document.getElementsByClassName("img");
 
+const btnToggle = document.createElement("BUTTON");
+const btnToggleText = document.createTextNode("Hide Image");
+btnToggle.appendChild(btnToggleText);
+body.appendChild(btnToggle)
+
+btnToggle.addEventListener("click", (e) => {
+  const im = document.getElementsByTagName("img");
+  
+  for (let i = 0; i <= im.length; i++) {
+    const element = im[i];
+    
+    if (element === undefined) {
+      return;
+    }
+
+    const parent = element.parentNode
+
+    if (parent.style.display === "none") {
+      parent.style.display = "block";
+      parent.previousElementSibling.style.display = "block";
+      btnToggle.textContent = "Hide Image";
+    } else {
+      parent.style.display = "none";
+      parent.previousElementSibling.style.display = "none";
+      btnToggle.textContent = "Show Image";
+    }
+
+  }
+  
+})
 
 const addParagraphs = () => {
   
   Array.prototype.forEach.call(paragraphs, (el) => {
-    let node = document.createElement("P");
-    let hr = document.createElement("HR");
+    
+    const node = document.createElement("P");
+    const hr = document.createElement("HR");
     node.innerHTML = el.innerHTML;
 
     body.insertBefore(hr, btn);
     body.insertBefore(node, btn);
 
-    return body;
-
   })
 
 }
+
+
+
+
+
+
+// Note
+// const addParagraphs = () => {
+  
+//   Array.prototype.forEach.call(paragraphs, (el) => {
+    
+//     // For btn
+//     if (el.contains(img)) {
+//       const node = document.createElement("P");
+//       const hr = document.createElement("HR");
+
+//       const btnNode = document.createElement("BUTTON");
+//       const btnTextNode = document.createTextNode("Toggle");
+//       btnNode.appendChild(btnTextNode);
+
+//       node.innerHTML = el.innerHTML;
+//       node.appendChild(btnNode)
+
+//       body.insertBefore(hr, btn);
+//       body.insertBefore(node, btn);
+
+//       btnNode.addEventListener("click", (e) => {
+//         console.log(btnNode.parentNode.parentNode);
+//         const pa = btnNode.parentNode;
+//         body.removeChild(pa)
+//       })
+
+//     } else {
+//       const node = document.createElement("P");
+//       const hr = document.createElement("HR");
+//       node.innerHTML = el.innerHTML;
+
+//       body.insertBefore(hr, btn);
+//       body.insertBefore(node, btn);
+//     }
+
+//   })
+
+// }
