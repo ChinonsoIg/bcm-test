fce4e2dbcbfd6e21f0dc34e759b1c70d497094ae
ChinonsoIg <ojononso@yahoo.com>
2021-12-08 21:33:03 +0100

    indented mainjs

--- html-test/main.js
+++ html-test/main.js
@@ -49,6 +49,7 @@ btnToggle.addEventListener("click", () => {
   
 });
 
+
 const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
